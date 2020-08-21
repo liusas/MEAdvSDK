06bc3f411e904d8e729ed948515ccde27f240f38
刘峰 <liufeng@mobiexchanger.com>
2020-08-21 14:54:17 +0800

    将初始化数据库拿到 sdk 中

--- MEAdvSDK/Classes/MEAdBaseManager.m
+++ MEAdvSDK/Classes/MEAdBaseManager.m
@@ -11,6 +11,7 @@
 #import "MEFeedAdManager.h"
 #import "MERewardedVideoManager.h"
 #import "MEInterstitialAdManager.h"
+#import <Realm.h>
 
 static  MEAdBaseManager *baseManager;
 static dispatch_once_t onceToken;
@@ -57,6 +58,22 @@ static dispatch_once_t onceToken;
 
 + (void)launchWithAppID:(NSString *)appid {
     [self launchWithAppID:appid finished:nil];
+    
+    // Override point for customization after application launch.
+    RLMRealmConfiguration *config = [RLMRealmConfiguration defaultConfiguration];
+    // 设置新的架构版本。这个版本号必须高于之前所用的版本号（如果您之前从未设置过架构版本，那么这个版本号设置为 0）
+    config.schemaVersion = 0;
+    // 设置闭包，这个闭包将会在打开低于上面所设置版本号的 Realm 数据库的时候被自动调用
+    config.migrationBlock = ^(RLMMigration *migration, uint64_t oldSchemaVersion) {
+        // 目前我们还未进行数据迁移，因此 oldSchemaVersion == 0
+        if (oldSchemaVersion < 1) {
+            // 什么都不要做！Realm 会自行检测新增和需要移除的属性，然后自动更新硬盘上的数据库架构
+        }
+    };
+    // 告诉 Realm 为默认的 Realm 数据库使用这个新的配置对象
+    [RLMRealmConfiguration setDefaultConfiguration:config];
+    // 现在我们已经告诉了 Realm 如何处理架构的变化，打开文件之后将会自动执行迁移
+    [RLMRealm defaultRealm];
 }
 
 + (void)launchWithAppID:(NSString *)appid finished:(RequestAndInitFinished)finished {
